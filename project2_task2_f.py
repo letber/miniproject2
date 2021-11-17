58db41fd53463edda7a0cb39ac2c0f45e1761746
letber <danylo.hoking@gmail.com>
2021-11-17 14:07:31 +0200

    added 2f

--- project2_task2_f.py
+++ project2_task2_f.py
@@ -28,16 +28,72 @@ def find_files(reg_file):
     good_files = []
     for root, dirs, files in os.walk('.'):
         for file in files:
-            if re.search(reg_file, file) != None:
+            if re.search(reg_file, file):
                 good_files.append(os.path.join(root, file))
 
     return good_files
 
 
 def find_patterns(files_list, pattern):
-    pass
+    good_list = []
+    for file in files_list:
+        with open(file, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            if re.search(pattern, line):
+                good_list.append([file, line, lines.index(line) + 1])
+
+    return good_list
+
+
+def show_files(f_list, show_lines, show_counts):
+    show_list = dict()
+    names = []
+    for name in f_list:
+        names.append(name[0])
+    if show_counts:
+        for file in names:
+            if file in show_list:
+                value = show_list[file]
+                show_list[file] = [value[0] + 1]
+            else:
+                show_list[file] = [1]
+
+        return show_list
+
+    if show_lines:
+        for file in f_list:
+            if file[0] in show_list:
+                value = show_list[file[0]]
+                value.append(str(file[2]) + ' ' + file[1])
+                show_list[file[0]] = value
+            else:
+                show_list[file[0]] = [str(file[2]) + ' ' + file[1]]
+
+        return show_list
+
+    for file in f_list:
+        if file[0] in show_list:
+            value = show_list[file[0]]
+            value.append(file[1])
+            show_list[file[0]] = value
+        else:
+            show_list[file[0]] = [file[1]]
+
+    return show_list
 
 
 if __name__ == '__main__':
     pars = create_parser()
     args = pars.parse_args()
+
+    lines = show_files(find_patterns(find_files(args.filename),
+                                     args.find), args.show_lines, args.only_show_counts)
+    for line in lines:
+        print(line)
+        for sub_line in lines[line]:
+            if not isinstance(sub_line, int):
+                print(sub_line.rstrip('\n'))
+            else:
+                print(sub_line)
+        print()
